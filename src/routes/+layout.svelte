bacb1d1b899799d8e4356c9b510f245b7d5693bd
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-03-17 10:05:50 +0100

    feat: view transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,50 @@
 <script>
 	export let data;
 	import { Menu, Footer } from '$lib/index.js';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
-
-<Menu/>
+<Menu />
 <slot />
 <Footer {data} />
+
+<style>
+	@keyframes strong-slide-in {
+		0% {
+			transform: translateY(40px);
+		}
+		100% {
+			transform: translateY(0);
+		}
+	}
+
+	@keyframes strong-slide-out {
+		0% {
+			transform: translateY(0);
+		}
+		100% {
+			transform: translateY(-20px);
+		}
+	}
+
+	@media (prefers-reduced-motion: no-preference) {
+		:root::view-transition-old(root) {
+			animation: 600ms cubic-bezier(0.4, 0, 0.2, 1) both strong-slide-out;
+		}
+
+		:root::view-transition-new(root) {
+			animation: 600ms cubic-bezier(0.4, 0, 0.2, 1) 100ms both strong-slide-in;
+		}
+	}
+</style>
