93b2a1ff14cadc60e44799c705ac8e7efc2c125b
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-03-19 11:54:32 +0100

    delete file

---
+++ receptstampot.js
@@ -0,0 +1,10 @@
+
+function stampot(ingredienten) {
+    console.log('Stampot met ' + ingredienten);
+    return ('Stampot met ' + ingredienten);
+}
+
+stampot('boerenkool');
+stampot('andijvie');
+stampot('hutspot');
+stampot('zuurkool');
