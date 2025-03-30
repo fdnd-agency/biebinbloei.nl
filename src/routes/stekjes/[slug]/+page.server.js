d878bbcb02af4c3351758641c66e0a8c2df3d617
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-03-31 00:19:50 +0200

    feat: buttons werken

--- src/routes/stekjes/[slug]/+page.server.js
+++ src/routes/stekjes/[slug]/+page.server.js
@@ -1,6 +1,6 @@
 import { gql } from 'graphql-request';
 import { hygraph } from '$lib/utils/hygraph.js';
-
+// qeury object
 export const load = async ({ params }) => {
 	let query = gql`
 		query Assets($slug: String) {
@@ -10,7 +10,7 @@ export const load = async ({ params }) => {
 				fotos {
 					url
 					width
-                  	height
+					height
 				}
 				giftig
 				landvanherkomst
@@ -21,11 +21,43 @@ export const load = async ({ params }) => {
 				verpotten
 				watergeven
 				zonlicht
+				createdAt
 			}
-			
 		}
 	`;
+	// Deze twee zijn qeury objects
+	let queryVolgende = gql`
+		query volgendStekje($currentCreationDate: DateTime!) {
+			stekjes(where: { createdAt_gt: $currentCreationDate }, orderBy: createdAt_ASC, first: 1) {
+				slug
+			}
+		}
+	`;
+
+	let queryVorige = gql`
+		query vorigStekje($currentCreationDate: DateTime!) {
+			stekjes(where: { createdAt_lt: $currentCreationDate }, orderBy: createdAt_DESC, first: 1) {
+				slug
+			}
+		}
+	`;
+
+	// hygraph (query request)
 	const slug = params.slug;
 	let result = await hygraph.request(query, { slug });
+
+	const currentCreationDate = result.stekje.createdAt;
+	let resultVolgende = await hygraph.request(queryVolgende, { currentCreationDate });
+	
+	let resultVorige = await hygraph.request(queryVorige, { currentCreationDate });
+	
+	if (resultVolgende.stekjes && resultVolgende.stekjes.length > 0) {
+		result.volgende = resultVolgende.stekjes[0].slug;
+	}
+
+	if (resultVorige.stekjes && resultVorige.stekjes.length > 0) {
+		result.vorige = resultVorige.stekjes[0].slug;
+	}
+
 	return result;
 };
