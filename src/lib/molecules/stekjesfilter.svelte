65ef132f3f7c8a305cc0931ddc58db1a51fe0aa8
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-05-14 21:30:06 +0200

    feat: file setup & button implementation :sparkles: :tada:

---
+++ src/lib/molecules/stekjesfilter.svelte
@@ -0,0 +1,27 @@
+<script>
+	let { data } = $props();
+</script>
+
+<section>
+    <button>Alle stekjes</button>
+    <button>Actuele stekjes</button>
+</section>
+
+<style>
+
+button {
+    background: var(--main-color-beige);
+    color: var(--main-color-brown);
+    font-family: 'Poppins-Medium';
+    border: none;
+    padding: 0.5em 1em;
+    border-radius: 0.5em;
+    cursor: pointer;
+}
+
+button:hover {
+    background: var(--main-color-brown);
+    color: var(--main-color-beige);
+    font-family: 'Poppins-Bold';
+}
+</style>
--- src/routes/stekjes/+page.svelte
+++ src/routes/stekjes/+page.svelte
@@ -1,18 +1,17 @@
 <script>
-    import { HeroHeaders, OverViewPages, IntroHeaders, ImageCarousel} from '$lib/index.js';
-  let { data } = $props();
-
+	import { HeroHeaders, OverViewPages, IntroHeaders, ImageCarousel } from '$lib/index.js';
+	import Stekjesfilter from '$lib/molecules/stekjesfilter.svelte';
+	let { data } = $props();
 </script>
 
-<HeroHeaders 
-    heroInfo={data.heroHeaders[0]}
-/>
+<HeroHeaders heroInfo={data.heroHeaders[0]} />
 <IntroHeaders heroInfo={data.heroHeaders[0]} />
 
-<ImageCarousel heroInfo={data.heroHeaders[0]} />
+<Stekjesfilter />
 
-<OverViewPages 
+<!-- <ImageCarousel heroInfo={data.heroHeaders[0]} /> -->
+
+<!-- <OverViewPages 
   viewPageData={data.overviewPages[0]} 
   
-  />
- 
+  /> -->
