b7407bcff4dd639a3f16fff48ca2b6a9c92d54a6
TristanKatte <tristankatte@hotmail.com>
2025-05-22 13:41:02 +0200

    server works

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import '../styles/global.css';
 	import { Menu, Footer } from '$lib/index.js';
 
 	import { onNavigate } from '$app/navigation';
@@ -17,36 +18,8 @@
 </script>
 
 
+
 <Menu/>
 {@render children?.()}
 <Footer {data} />
 
-<style>
-	@keyframes strong-slide-in {
-		0% {
-			transform: translateY(40px);
-		}
-		100% {
-			transform: translateY(0);
-		}
-	}
-
-	@keyframes strong-slide-out {
-		0% {
-			transform: translateY(0);
-		}
-		100% {
-			transform: translateY(-20px);
-		}
-	}
-
-	@media (prefers-reduced-motion: no-preference) {
-		:root::view-transition-old(root) {
-			animation: 600ms cubic-bezier(0.4, 0, 0.2, 1) both strong-slide-out;
-		}
-
-		:root::view-transition-new(root) {
-			animation: 600ms cubic-bezier(0.4, 0, 0.2, 1) 100ms both strong-slide-in;
-		}
-	}
-</style>
