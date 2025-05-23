fb662f22e13e384948e94e8a598fe1c529bf8eeb
wingsvn <wing-san.lee@live.nl>
2025-05-23 15:11:50 +0200

    V6 comments verwijderen

--- src/lib/atoms/buttons/button.svelte
+++ src/lib/atoms/buttons/button.svelte
@@ -13,12 +13,6 @@
 	<a {href} class={buttonClass}>
 		{buttonText}
 		<span>
-			<!-- <svg class={svgFill} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 44 47">
-				<path
-					d="M40.492 18.788 8.942 1.034C4.944-1.218 0 1.673 0 6.262v34.704c0 4.537 4.842 7.433 8.84 5.285l31.549-16.948c4.167-2.239 4.225-8.195.103-10.515Z"
-					fill={svgFill}
-				/>
-			</svg> -->
 			<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path d="M17.154 11.4385L11.554 5.83848L12.979 4.43848L20.979 12.4385L12.979 20.4385L11.554 19.0385L17.154 13.4385L4.979 13.4385L4.979 11.4385L17.154 11.4385Z" fill="currentColor"/>
 			</svg>
@@ -27,7 +21,6 @@
 </div>
 
 <style>
-	/* button style */
 	a {
 		background-color: var(--color-accent);
 		--color-accent: var(--accent, --main-color-brown);
@@ -53,7 +46,6 @@
 	.button-wrapper {
 		display: flex;
 		flex-direction: column;
-		/* position: relative; */
 		align-items: center;
 		position: absolute;
 		right: 1em;
@@ -99,26 +91,12 @@
 		--color-text: var(--card-color-green);
 	}
 
-	/* button hover */
-	/* a:hover {
-		cursor: pointer;
-		transform: scale(1.05);
-		--color-accent: var(--text-color-white);
-		--color-text: var(--main-color-orange);
-	}
-	a:hover .svg-beige,
-	a:hover .svg-green {
-		--svg-fill: var(--main-color-orange);
-	} */
-
 	a:hover {
 		cursor: pointer;
 		transform: scale(0.9);
-		/* background-color: #270e01; */
 	}
 	
 	/* svg style */
-	
 	span {
 		display: flex;
 		align-items: center;
