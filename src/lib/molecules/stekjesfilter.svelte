f62f354c54e34ec9d874fd327ca6f47835cba222
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-06-01 15:57:11 +0200

    feat: added html & css h2 and p

--- src/lib/molecules/stekjesfilter.svelte
+++ src/lib/molecules/stekjesfilter.svelte
@@ -11,9 +11,22 @@
 	function currentStekjes() {
 		dispatch('showCurrent');
 	}
+
+	const fullText = data.sectionInfos[0].description;
+	const [mainText, afterDash] = fullText.split('-- ');
 </script>
 
 <section>
+	<h2>
+		<span class="outline">{data.sectionInfos[0].title.split(' ')[0]}</span>
+		<span class="block">{data.sectionInfos[0].title.split(' ')[1]}</span>
+	</h2>
+
+	<p>
+		{mainText}
+		<span class="block bold">-- {afterDash}</span>
+	</p>
+
 	<button onclick={allStekjes} class="no-js-disabled" class:active={active === 'alle'}
 		>Alle stekjes</button
 	>
@@ -21,7 +34,7 @@
 </section>
 
 <!-- -- js-disabled styling -- -->
- 
+
 <noscript>
 	<style>
 		.no-js-disabled {
@@ -39,6 +52,36 @@
 		padding: 1rem;
 		margin-top: 2rem;
 		margin: 1.25rem 0;
+	}
+
+	h2 {
+		font-size: 2rem;
+		text-transform: uppercase;
+		font-family: var(--header-font);
+		color: var(--main-color-brown);
+	}
+
+	.outline {
+		color: transparent;
+		-webkit-text-stroke: 1.8px var(--main-color-brown);
+	}
+
+	.block {
+		display: block;
+		line-height: 2rem;
+	}
+
+	p {
+		font-size: 1rem;
+		margin-top: 1rem;
+		line-height: 1.6rem;
+		color: var(--main-color-brown);
+		font-family: var(--link-font);
+	}
+
+	.block {
+		display: block;
+		font-family: var(--sub-header-font);
 	}
 
 	/* -- Button styling -- */
@@ -81,5 +124,37 @@
 	button:focus-visible {
 		outline-offset: 2px;
 		outline: 3px solid var(--main-color-brown);
+	}
+
+	/* -- Media query styling -- */
+
+	/* -- Tablet styling -- */
+
+	@media (min-width: 48rem) {
+		section {
+			margin: 5.25rem 0 3.25rem 0;
+			padding: 1.25rem;
+		}
+
+		p {
+			max-width: 25em;
+		}
+	}
+
+	/* -- Desktop styling -- */
+
+	@media (min-width: 74rem) {
+		section {
+			margin: 5.5rem 0 3.5rem 0;
+			padding: 1.5rem;
+		}
+
+		h2 {
+			font-size: 2.25rem;
+		}
+
+		p {
+			max-width: 31.25rem;
+		}
 	}
 </style>
